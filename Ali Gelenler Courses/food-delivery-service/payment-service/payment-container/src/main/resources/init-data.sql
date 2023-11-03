e845d064b05b0b95497768640112dc0204d56206
kbhatt23 <kanishkeminem@gmail.com>
2023-11-03 17:13:18 +0530

    upgrade to spring boot 2.7.5

--- Ali Gelenler Courses/food-delivery-service/payment-service/payment-container/src/main/resources/init-data.sql
+++ Ali Gelenler Courses/food-delivery-service/payment-service/payment-container/src/main/resources/init-data.sql
@@ -1,11 +1,12 @@
 INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
-	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb21', 'ea4257d0-f887-4c3b-bfa0-4b6ef2728972', 500.00);
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb21', 'c017b79c-8a16-4a22-b291-dfed7c752b4b', 500.00);
 INSERT INTO payment.credit_history(id, customer_id, amount, type)
-	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb23', 'ea4257d0-f887-4c3b-bfa0-4b6ef2728972', 100.00, 'CREDIT');
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb23', 'c017b79c-8a16-4a22-b291-dfed7c752b4b', 100.00, 'CREDIT');
 INSERT INTO payment.credit_history(id, customer_id, amount, type)
-	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb24', 'ea4257d0-f887-4c3b-bfa0-4b6ef2728972', 600.00, 'CREDIT');
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb24', 'c017b79c-8a16-4a22-b291-dfed7c752b4b', 600.00, 'CREDIT');
 INSERT INTO payment.credit_history(id, customer_id, amount, type)
-	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb25', 'ea4257d0-f887-4c3b-bfa0-4b6ef2728972', 200.00, 'DEBIT');
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb25', 'c017b79c-8a16-4a22-b291-dfed7c752b4b', 200.00, 'DEBIT');
+
 
 
 INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
