d168fbf2084469bed167bd6a854cc530f3fef5d0
kbhatt23 <kanishkeminem@gmail.com>
2023-11-06 11:00:07 +0530

    debezium changes + db locking during jmeter load test

--- Ali Gelenler Courses/food-delivery-service/payment-service/payment-container/src/main/resources/init-data.sql
+++ Ali Gelenler Courses/food-delivery-service/payment-service/payment-container/src/main/resources/init-data.sql
@@ -1,11 +1,11 @@
 INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
-	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb21', 'c017b79c-8a16-4a22-b291-dfed7c752b4b', 500.00);
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb21', '515ff14e-e252-4653-940f-9d9d868654e0', 50001.00);
 INSERT INTO payment.credit_history(id, customer_id, amount, type)
-	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb23', 'c017b79c-8a16-4a22-b291-dfed7c752b4b', 100.00, 'CREDIT');
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb23', '515ff14e-e252-4653-940f-9d9d868654e0', 10001.00, 'CREDIT');
 INSERT INTO payment.credit_history(id, customer_id, amount, type)
-	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb24', 'c017b79c-8a16-4a22-b291-dfed7c752b4b', 600.00, 'CREDIT');
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb24', '515ff14e-e252-4653-940f-9d9d868654e0', 60000.00, 'CREDIT');
 INSERT INTO payment.credit_history(id, customer_id, amount, type)
-	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb25', 'c017b79c-8a16-4a22-b291-dfed7c752b4b', 200.00, 'DEBIT');
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb25', '515ff14e-e252-4653-940f-9d9d868654e0', 20000.00, 'DEBIT');
 
 
 
--- Ali Gelenler Courses/food-delivery-service/payment-service/payment-dataaccess/src/main/java/com/learning/paymentdataaccess/repositories/CreditHistoryJPARepository.java
+++ Ali Gelenler Courses/food-delivery-service/payment-service/payment-dataaccess/src/main/java/com/learning/paymentdataaccess/repositories/CreditHistoryJPARepository.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import javax.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import com.learning.paymentdataaccess.entities.CreditHistoryEntity;
@@ -12,5 +15,6 @@ import com.learning.paymentdataaccess.entities.CreditHistoryEntity;
 @Repository
 public interface CreditHistoryJPARepository extends JpaRepository<CreditHistoryEntity, UUID> {
 
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	Optional<List<CreditHistoryEntity>> findByCustomerId(UUID customerId);
 }
